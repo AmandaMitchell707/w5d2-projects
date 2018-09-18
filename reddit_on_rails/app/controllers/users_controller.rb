22dac9aca8e201ff5493f613c6f8edd1296f78f7
atar97 <austin.cotant@gmail.com>
2018-09-18 11:47:30 -0700

    Add user and session controllers

--- reddit_on_rails/app/controllers/users_controller.rb
+++ reddit_on_rails/app/controllers/users_controller.rb
@@ -1,7 +1,34 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show]
+  
   def new
+    @user = User.new
+    render :new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      redirect_to user_url(@user)
+    else 
+      now_errors(@user)
+      render :new
+    end 
   end
+  
+  def show 
+    render :show
+  end 
+  
+  private 
+  
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end 
+  
+  def find_user
+    @user = User.find(params[:id])
+  end 
+  
 end
